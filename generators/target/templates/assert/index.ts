cb7c56a4250f74f04c2116f3cc022903cb2537c4
Matthew Searles <matt_2304@hotmail.com>
2016-06-18 07:58:25 +1000

    provide itAsync in assert class to expose karma's done callback

--- generators/target/templates/assert/index.ts
+++ generators/target/templates/assert/index.ts
@@ -7,17 +7,33 @@ import { Type } from '@angular/compiler/src/facade/lang';
 */ 
  
 /* istanbul ignore next */
-export class Assert<TComponent> {    
-    constructor(private type : Type, private overrideProviders : any[] = []) {        
+export class Assert<TComponent> {
+    constructor(private type : Type, private overrideProviders : any[] = []) {
     }
-    
+
     public it(name : string, fn : (component : TComponent, element : any, fixture : ComponentFixture<TComponent>) => void) {            
-        return it(name, inject( [TestComponentBuilder], (testComponentBuilder : TestComponentBuilder) => {            
+        return it(name, inject( [TestComponentBuilder], (testComponentBuilder : TestComponentBuilder) => {
             return testComponentBuilder
                 .overrideProviders(this.type, this.overrideProviders)
-                .createAsync(this.type).then(fixture => {                                                
+                .createAsync(this.type).then(fixture => {
                     fn(fixture.debugElement.componentInstance, fixture.debugElement.nativeElement, fixture);                            
                 });
-        })); 
+        }));
+    }
+
+    public itAsync(name : string, fn : (done : any, component : TComponent, element : any, fixture : ComponentFixture<TComponent>) => void) {            
+        let doneCallback = null;
+        let injectFn = inject([TestComponentBuilder], (testComponentBuilder : TestComponentBuilder) => {
+            return testComponentBuilder
+                .overrideProviders(this.type, this.overrideProviders)
+                .createAsync(this.type).then(fixture => {
+                    fn(doneCallback, fixture.debugElement.componentInstance, fixture.debugElement.nativeElement, fixture);                            
+                });
+        });
+
+        return it(name, (done) => {
+            injectFn();
+            doneCallback = done;
+        });
     }
 }
